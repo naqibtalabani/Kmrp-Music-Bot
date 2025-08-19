7508937c38e69d63abe796ec864cc4523703f852
naqibtalabani <naqibtalabani4@gmail.com>
2025-08-19 15:26:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://NaQiB:123sarwat@cluster0.mylnjix.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
